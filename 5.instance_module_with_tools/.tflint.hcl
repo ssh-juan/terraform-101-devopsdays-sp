5323832867ea9ecf2de4f7de7555c964875b9f82
Juan Borges <juanbsoares@outlook.com>
2025-10-27 20:06:19 -0300

    first test tools pipeline

---
+++ 5.instance_module_with_tools/.tflint.hcl
@@ -0,0 +1,11 @@
+plugin "aws" {
+  enabled = true
+  version = "0.35.0"
+}
+
+config {
+  format = "compact"
+  module = true
+  ignore_module = false
+}
+t
---
+++ 5.instance_module_with_tools/module/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "website" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags          = merge(var.default_tags,
+                         {
+                          ami = var.ami
+                          },
+                           var.tags)
+}
