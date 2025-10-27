a8ca50153621b70c9543195ad4b347021980dc11
Juan Borges <juanbsoares@outlook.com>
2025-10-27 20:59:42 -0300

    refactor 3

--- 5.instance_module_with_tools/validate_variables.py
+++
@@ -1,8 +0,0 @@
-# validate_variables.py
-import os
-
-required_vars = ["ENVIRONMENT", "INSTANCE_TYPE"]
-
-for var in required_vars:
-    if var not in os.environ:
-        print(f"[WARN] {var} não configurada")
