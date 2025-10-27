e6be2962a87d8f59535e5e40f549b3f626f1c733
Juan Borges <juanbsoares@outlook.com>
2025-10-27 20:06:06 -0300

    commenting backend

--- 3.module_instance/backend.tf
+++ 3.module_instance/backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "s3" {
-    bucket = "juan-borges-terraform-state-xyzxyz"
-    key    = "module_instance/terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "juan-borges-terraform-state-xyzxyz"
+#     key    = "module_instance/terraform.tfstate"
+#   }
+# }
