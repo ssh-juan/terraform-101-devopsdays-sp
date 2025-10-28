0a75ccaecfc17b3a34165d78e75ccfb4eab95c17
Juan Borges <juanbsoares@outlook.com>
2025-10-27 22:18:44 -0300

    refactor to infracost 4

--- 5.instance_module_with_tools/versions.tf
+++ 5.instance_module_with_tools/versions.tf
@@ -12,3 +12,11 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "fake"
+  secret_key                  = "fake"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
