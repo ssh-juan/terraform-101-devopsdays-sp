7085de258d345a8129ead5543afdc632388e0ad0
Juan Borges <juanbsoares@outlook.com>
2025-10-27 21:15:04 -0300

    fix 106

--- 5.instance_module_with_tools/module/versions.tf
+++ 5.instance_module_with_tools/module/versions.tf
@@ -1,13 +1,14 @@
 terraform {
-  required_version = ">=1.0.0"
+  required_version = ">= 1.7.5"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      required_providers = ">= 4.0.0"
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
     }
     random = {
-      source = "hashicorp/random"
-      required_providers = ">= 1.0.0"
+      source  = "hashicorp/random"
+      version = ">= 3.5.0"
     }
   }
 }
